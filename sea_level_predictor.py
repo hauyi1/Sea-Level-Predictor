300f6a46efcf91916fbc220ea9950937644da808
Louise Wong <hauying101@gmail.com>
2024-08-02 16:05:35 +0000

    Added scatter graph

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -2,15 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
+
 def draw_plot():
     # Read data from file
+    df = pd.read_csv('epa-sea-level.csv')
 
 
     # Create scatter plot
+    regression = stats.linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], label='data')
+
 
 
     # Create first line of best fit
-
+    plt.plot(df['Year'], regression.intercept + regression.slope*df['Year'], label='Line of best fit')
 
     # Create second line of best fit
 
