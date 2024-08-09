3bb45a65014a7d6c4ea9e2bb028a1b27bf9855f2
hauyi1 <57042419+hauyi1@users.noreply.github.com>
2024-08-09 16:58:17 +0100

    Update sea_level_predictor.py
    Added both lines correctly

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -9,16 +9,22 @@ def draw_plot():
 
 
     # Create scatter plot
-    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], label='data')
-
+    plt.scatter(x, y, label='data')
+    
     # Create first line of best fit
-    regression = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
-    plt.plot(df['Year'], regression.intercept + regression.slope*df['Year'], label='Line of best fit')
-
+    regression = linregress(x, y)
+    x_prediction = pd.Series([i for i in range(1880, 2051)])
+    y_prediction = regression.intercept + regression.slope*x_prediction
+    plt.plot(x_prediction, y_prediction, label='Line of best fit')
+    
     # Create second line of best fit
     df_recent = df[df['Year']>=2000]
-    regress2 = linregress(df_recent['Year'], df_recent['CSIRO Adjusted Sea Level'])
-    plt.plot(df_recent['Year'], regress2.intercept + regress2.slope*df_recent['Year'], label='Line of best fit')
+    y_2 = df_recent['CSIRO Adjusted Sea Level']
+    x_2 = df_recent['Year']
+    regression_2 = linregress(x_2, y_2)
+    x_prediction_2 = pd.Series([i for i in range(2000, 2051)])
+    y_prediction_2 = regression_2.intercept + regression_2.slope * x_prediction_2
+    plt.plot(x_prediction_2, y_prediction_2, label='Line of best fit')
 
     # Add labels and title
     plt.xlabel('Year')
